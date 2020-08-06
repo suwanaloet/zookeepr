f6e8001f631885cb5b415046caa3829d64dae351
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-08-06 15:06:31 -0700

    still cannot GET

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
